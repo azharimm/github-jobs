01d756465c170cad2d4408fc800834ef15ea9ecd
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-06 10:23:24 +0700

    + job component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Container } from 'react-bootstrap'
-import useFetch from './hooks/useFetch'
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import useFetch from './hooks/useFetch';
+import Jobs from './components/Jobs';
 
 function App() {
-    const { jobs, loading, error } = useFetch()
+    const [params, setParams] = useState({});
+    const [page, setPage] = useState(1);
+    const { jobs, loading, error } = useFetch(params, page);
 
     return (
         <Container>
-            { loading && <h1>Loading..</h1>}
-            { error && <h1>Something Went Wrong! Please Try Again.</h1>}
-            <h1>{ jobs.length }</h1>
+            {loading && <h1>Loading..</h1>}
+            {error && <h1>Something Went Wrong! Please Try Again.</h1>}
+            {jobs.map(job => {
+                return <Jobs key={job.id} job={job}/>
+            })}
         </Container>
     );
 }
---
+++ src/components/Jobs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Jobs({job}) {
+    return (
+        <div>
+            {job.title}
+        </div>
+    )
+}
+
+export default Jobs
